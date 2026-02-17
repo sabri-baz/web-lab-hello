480fd217c4f3cf34320e2283681652eb8c8f38ef
Sabri Baz <sabribaz02@gmail.com>
2026-02-17 10:24:56 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: [sabri baz ]</p>
+      <p>Ogrenci No: [240541164]</p>
+    </div>
+  );
+}
+
+export default App;
